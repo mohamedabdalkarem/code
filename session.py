9db8b06b02ee87cb63d1fdeaec3e9b13881e0faa
mohmmed abdalkareem <mohamedabdalkareem976@gmail.com>
2025-02-18 15:40:00 +0200

    second commit

--- session.py
+++ session.py
@@ -4,3 +4,5 @@
 
 # "Adult" if age is above 18.
 age=int(input("Enter your age: "))
+print("Child" if age<12 else "Teenager" if age>=12 and age<=18 else "Adult")
+print("Child" if age<12 else "Teenager" if age>=12 and age<=18 else "Adult")    
