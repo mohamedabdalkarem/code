2baf86bf5ddd8b167261c040bf4d487dc3c8d9fa
mohamedabdalkareem <153396195+mohamedabdalkareem@users.noreply.github.com>
2025-02-27 10:40:04 +0200

    commit

--- session.py
+++ session.py
@@ -1,18 +1,7 @@
-operators = ['+', '-', '*', '/']
-expression = input("Enter an expression ") 
-for operator in operators:
-    if operator in expression:
-        num1, num2 = expression.split(operator)
-        num1, num2 = float(num1), float(num2)
-        
-        if operator == '+':
-            result = num1 + num2
-        elif operator == '-':
-            result = num1 - num2
-        elif operator == '*':
-            result = num1 * num2
-        elif operator == '/':
-            result = num1 / num2
-        
-        print(f"Result: {result}")
-        break
+n=10
+def x():
+    n=5
+    print(n)
+
+x()
+print(n)
